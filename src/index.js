42cf792376845d7204240df7c110e15b07d83ee9
Alexandros Karamouchtaris <alexkaramuxtaris@gmail.com>
2023-08-26 21:15:48 +0300

    Merge js files into App.js.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Card from './Card';
-import CardList from './CardList';
+import App from './App';
 import 'tachyons';
 import reportWebVitals from './reportWebVitals';
 import {robots} from './robots';
 
 const root = document.getElementById('root'); // Get the root element
 
-ReactDOM.createRoot(root).render(<CardList robots={robots} /> ); // Render the app
+ReactDOM.createRoot(root).render(<App />); // Render the app
