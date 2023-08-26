6ca7ff17e67c407b63e9ac29c81578b6901d2b40
Alexandros Karamouchtaris <alexkaramuxtaris@gmail.com>
2023-08-26 20:10:40 +0300

    Initial commit.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom'; // Correct import
 import './index.css';
-import App from './App';
+import Card from './Card';
+import 'tachyons';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = document.getElementById('root'); // Get the root element
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.createRoot(root).render(<Card />); // Render the app
