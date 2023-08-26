2cf0be155f4352a953a89ae2d28e28a83eff2e9c
Alexandros Karamouchtaris <alexkaramuxtaris@gmail.com>
2023-08-26 20:36:29 +0300

    Create add component.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom'; // Correct import
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import Card from './Card';
 import 'tachyons';
 import reportWebVitals from './reportWebVitals';
+import {robots} from './robots';
 
 const root = document.getElementById('root'); // Get the root element
 
-ReactDOM.createRoot(root).render(<Card />); // Render the app
+ReactDOM.createRoot(root).render(
+                                 <div>
+                                    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email } />
+                                    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email } />
+                                    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email } />
+                                 </div>
+                                ); // Render the app
