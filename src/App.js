79cfb6053db21c4a170acb32f54f9ee6024eef32
Alexandros Karamouchtaris <alexkaramuxtaris@gmail.com>
2023-08-27 11:46:36 +0300

    Add scroll effect.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './Searchbox';
 import './App.css';
+import Scroll from './Scroll';
 
 class App extends Component {
     constructor() {
@@ -27,13 +28,19 @@ class App extends Component {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
 
-        return (
-            <div className='tc'>
-                <h1 className='f2'>RoboFriends</h1>
-                <SearchBox searchChange = {this.onSearchChange} />
-                <CardList robots={filteredRobots} />
-            </div>
-        );
+        if (this.state.robots.length === 0) {
+            return <h1 className='flex justify-center items-center vh-100 ma0 tc'>Loading...</h1>
+        } else {
+            return (
+                <div className='tc'>
+                    <h1 className='f2'>RoboFriends</h1>
+                    <SearchBox searchChange = {this.onSearchChange} />
+                    <Scroll>
+                        <CardList robots={filteredRobots} />
+                    </Scroll>
+                </div>
+            );
+        }
     }
     
 }
